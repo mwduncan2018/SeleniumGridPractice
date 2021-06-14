fac45cc9bbeff14e52d830416923d2188299ffc7
Michael Duncan (Ubuntu1) <mwduncan2018@gmail.com>
2021-06-14 10:14:51 -0400

    updates

--- src/test/java/dunit/grid/GettingStarted1.java
+++ src/test/java/dunit/grid/GettingStarted1.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -23,7 +24,43 @@ public class GettingStarted1 {
 		WebDriver driver = new RemoteWebDriver(new URL(url), dc);
 		driver.get("https://www.ultimateguitar.com");
 		System.out.println("..." + driver.getCurrentUrl());
-		Thread.sleep(5000);
-		driver.close();				
+		driver.quit();
 	}
+
+	@Test
+	public void betaTest() throws Exception {
+		String url = "http://172.29.210.59:4444/wd/hub";
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("firefox");
+		dc.setPlatform(Platform.LINUX);
+		WebDriver driver = new RemoteWebDriver(new URL(url), dc);
+		driver.get("https://www.duckduckgo.com");
+		System.out.println("..." + driver.getCurrentUrl());
+		driver.quit();
+	}
+
+	@Test
+	public void charlieTest() throws Exception {
+		String url = "http://172.29.210.59:4444/wd/hub";
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("firefox");
+		dc.setPlatform(Platform.LINUX);
+		WebDriver driver = new RemoteWebDriver(new URL(url), dc);
+		driver.get("https://www.google.com");
+		System.out.println("..." + driver.getCurrentUrl());
+		driver.quit();
+	}
+
+	@Test
+	public void deltaTest() throws Exception {
+		String url = "http://172.29.210.59:4444/wd/hub";
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("firefox");
+		dc.setPlatform(Platform.LINUX);
+		WebDriver driver = new RemoteWebDriver(new URL(url), dc);
+		driver.get("https://www.yahoo.com");
+		System.out.println("..." + driver.getCurrentUrl());
+		driver.quit();
+	}
+
 }
